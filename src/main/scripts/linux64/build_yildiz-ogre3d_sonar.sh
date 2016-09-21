1d85f64c1c5e2cfec41dab283645843d73af4445
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2016-09-21 09:48:21 +0200

    [YIL-71] Add sonar cpp.

---
+++ src/main/scripts/linux64/build_yildiz-ogre3d_sonar.sh
@@ -0,0 +1,8 @@
+cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH=/usr/share/OGRE/cmake/modules -DLIBRARY_OUTPUT_PATH="../../../../target/classes/linux64" -G "Unix Makefiles"
+
+build-wrapper-linux-x86-64 --out-dir ../../../../target/sonar-cpp make
+
+rm -R CMakeFiles
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm Makefile
