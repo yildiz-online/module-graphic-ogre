cbae3dc46fa4522f56d2b08293e17150f470ccdc
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-09-11 17:12:08 +0200

    [YIL-324] Make the build fail if native compilation fails.

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -1,8 +1,11 @@
 cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH=/usr/share/OGRE/cmake/modules -DLIBRARY_OUTPUT_PATH="../../../../target/classes/linux64" -G "Unix Makefiles"
 
 make
+r1=$?
 
 rm -R CMakeFiles
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
+
+return $r1
--- src/main/scripts/win32/build.sh
+++ src/main/scripts/win32/build.sh
@@ -1,6 +1,7 @@
 cmake . -DCMAKE_BUILD_TYPE=Release -DLIBRARY_OUTPUT_PATH="../../../../target/classes/win32" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 make
+r1=$?
 
 rm -R CMakeFiles
 rm Makefile
@@ -15,5 +16,7 @@ cp ogre/bin/RenderSystem_GL.dll ../../../../target/classes/win32/RenderSystem_GL
 cp physfs/bin/libphysfs.dll ../../../../target/classes/win32/libphysfs.dll
 cp libstdc++-6.dll ../../../../target/classes/win32/libstdc++-6.dll
 cp libgcc_s_sjlj-1.dll ../../../../target/classes/win32/libgcc_s_sjlj-1.dll
+
+rm $r1
 
 
