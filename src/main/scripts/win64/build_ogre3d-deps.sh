087204f950f17e4b2621d5810d790a7b1d287a02
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-02-04 10:37:39 +0100

    [YMGO-0] Remove Ogre build.

--- src/main/scripts/win64/build_ogre3d-deps.sh
+++
@@ -1,9 +0,0 @@
-mkdir src/main/scripts/ogredeps/build
-cd src/main/scripts/ogredeps/build
-
-cmake . -DCMAKE_BUILD_TYPE="Release" -DOGREDEPS_BUILD_RAPIDJSON=FALSE -DOGREDEPS_BUILD_SDL2=FALSE -DOGREDEPS_BUILD_ZZIPLIB=FALSE -DOGREDEPS_BUILD_AMD_QBS=FALSE -DOGREDEPS_BUILD_NVIDIA_NVAPI=FALSE -DCMAKE_INSTALL_PREFIX="Dependencies/" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
-
-make install
-make clean
-
-rm -R *
