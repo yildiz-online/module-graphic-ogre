45aa0338ad66b2b26e04878000e0f92625e36aa6
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-02-05 13:46:08 +0100

    [YMGO-0] Update copyright.

--- src/main/java/be/yildiz/module/graphic/ogre/OgreWorld.java
+++ src/main/java/be/yildiz/module/graphic/ogre/OgreWorld.java
@@ -1,30 +1,50 @@
 /*
  * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
  *
- * Copyright (c) 2017 Grégory Van den Borre
+ *  Copyright (c) 2018 Grégory Van den Borre
  *
- * More infos available: https://www.yildiz-games.be
+ *  More infos available: https://www.yildiz-games.be
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including without
- * limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all copies or substantial
- * portions of the Software.
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial
+ *  portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
- * OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ *
  */
 
 package be.yildiz.module.graphic.ogre;
 
 import be.yildiz.module.color.Color;
-import be.yildiz.module.graphic.*;
+import be.yildiz.module.graphic.AbstractCamera;
+import be.yildiz.module.graphic.AbstractLight;
+import be.yildiz.module.graphic.AbstractParticleSystem;
+import be.yildiz.module.graphic.BillboardSet;
+import be.yildiz.module.graphic.DirectionalLight;
+import be.yildiz.module.graphic.ElectricArc;
+import be.yildiz.module.graphic.Explosion;
+import be.yildiz.module.graphic.Font;
+import be.yildiz.module.graphic.GraphicMesh;
+import be.yildiz.module.graphic.GraphicObject;
+import be.yildiz.module.graphic.GraphicWorld;
+import be.yildiz.module.graphic.LensFlare;
+import be.yildiz.module.graphic.Line;
+import be.yildiz.module.graphic.Material;
+import be.yildiz.module.graphic.MovableText;
+import be.yildiz.module.graphic.Ocean;
+import be.yildiz.module.graphic.PointLight;
+import be.yildiz.module.graphic.Sky;
+import be.yildiz.module.graphic.Skybox;
+import be.yildiz.module.graphic.SpotLight;
 import be.yildizgames.common.geometry.Point3D;
 import be.yildizgames.common.model.EntityId;
 import be.yildizgames.common.shape.Box;
