82acc2b41d732ebc5617c1cbddd0e82965cc8773
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-03-10 13:41:41 +0100

    [YMGO-0] Fix build.

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,12 +1,13 @@
-mkdir src/main/scripts/win64/build
-cd src/main/scripts/win64/build
+mkdir $PWD/build
+cd build
 
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_COLOR_MAKEFILE=on -DLIBRARY_OUTPUT_PATH="../../../../../target/classes/win64" -DCMAKE_TOOLCHAIN_FILE=../../mingw-toolchain.cmake
+cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_COLOR_MAKEFILE=on -DLIBRARY_OUTPUT_PATH="../../../../../target/classes/win64" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 make
 r1=$?
 
-rm -R *
+cd ..
+rm -R build
 
 return $r1
 
