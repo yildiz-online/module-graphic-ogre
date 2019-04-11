004556797548ea9c7a7644b7a6c420d1fe6c7013
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-04-11 07:36:07 +0200

    [WIP] Use Vfs module.

--- src/main/resources/main/includes/yz_ogre_vfs_Archive.hpp
+++ src/main/resources/main/includes/yz_ogre_vfs_Archive.hpp
@@ -39,7 +39,7 @@ class Archive: public Ogre::Archive {
 
 public:
 
-    Archive(yz::physfs::Wrapper* vfs, const Ogre::String& name, const Ogre::String& type) : Ogre::Archive(name, type), this->vfs(vfs) {
+    Archive(yz::physfs::Wrapper* vfs, const Ogre::String& name, const Ogre::String& type) : Ogre::Archive(name, type), vfs(vfs) {
         LOG_FUNCTION
     }
 
