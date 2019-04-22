f90aa7295472551a98681404fce07e4378e3d711
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-04-22 21:04:05 +0200

    [WIP] Interface.

--- src/main/resources/main/c++/Root.cpp
+++ src/main/resources/main/c++/Root.cpp
@@ -102,7 +102,7 @@ void yz::Root::addResourcePath(const std::string& name, const std::string& path,
             Ogre::ResourceGroupManager::getSingleton().addResourceLocation(path, "Zip", name, false);
             break;
         case 2:
-            this->vfs->registerContainer(name);
+            this->vfs->registerContainer(path);
             Ogre::ResourceGroupManager::getSingleton().addResourceLocation("", "Package", name, false);
             break;
         case 3:
