a4e550bdccf20e027ba687d630ff4f5a747c4de0
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-07-23 13:53:52 +0200

    Update Node.hpp

--- src/main/resources/main/includes/Node.hpp
+++ src/main/resources/main/includes/Node.hpp
@@ -322,15 +322,15 @@ private:
 
     void destroyAllAttachedMovableObjects(Ogre::SceneNode* i_pSceneNode) {
         // Destroy all the attached objects
-        Ogre::SceneNode::ObjectIterator itObject = i_pSceneNode->getAttachedObjectIterator();
-		//std::vector<Ogre::Movable*> objects = i_pSceneNode->getAttachedObjects();
-		//for (int i = 0; i < objects.size(); i++) {
-		//	i_pSceneNode->getCreator()->destroyMovableObject(objects.at(i));
-		//}
-		while (itObject.hasMoreElements()) {
+        //Ogre::SceneNode::ObjectIterator itObject = i_pSceneNode->getAttachedObjectIterator();
+        Ogre::SceneNode::ObjectMap objects = i_pSceneNode->getAttachedObjects();
+        for (int i = 0; i < objects.size(); i++) {
+            i_pSceneNode->getCreator()->destroyMovableObject(objects.at(i));
+        }
+		/*while (itObject.hasMoreElements()) {
 		    Ogre::MovableObject* pObject = static_cast<Ogre::MovableObject*>(itObject.getNext());
 		    i_pSceneNode->getCreator()->destroyMovableObject(pObject);
-		}
+		}*/
 		// Recurse to child SceneNodes
 		//Ogre::ChildNodeMap children = i_pSceneNode->getChildren();
 		/*for (int i = 0; i < children.size(); i++) {
